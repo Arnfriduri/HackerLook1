6df32f2b9a30f8dea04318766deb5879242b0f1c
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2017-09-24 22:50:42 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerLook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Height = Console.WindowHeight;
+            int Width = Console.WindowWidth;
+            string Version = "Version 1.0";
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            //Use SetWindowSize so we can control the size of the window
+            //Console.SetWindowSize(70, 30);
+            //It is called Character Map to create the layout.
+            //Console.SetCursorPosition(10, 2);
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("             ##   ##  #######  #######  ##   ##  #######  #######             ");
+            Console.WriteLine("             ##   ##  ##   ##  ##       ##  ##   ##       ##  ##              ");
+            Console.WriteLine("             #######  #######  ##       #####    #####    #####               ");
+            Console.WriteLine("             ##   ##  ##   ##  ##       ##  ##   ##       ##  ##              ");
+            Console.WriteLine("             ##   ##  ##   ##  #######  ##   ##  #######  ##   ##             ");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.SetCursorPosition((Width-Version.Length)/2, 7);
+            Console.WriteLine(Version);
+            Console.ReadKey();
+        }
+    }
+}
