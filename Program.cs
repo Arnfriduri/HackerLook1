9c1cbb3e33054f79e45de0abdf46b94cabc54693
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2017-09-26 22:52:05 +0000

    Graphic change
    Made some graphic changes for the console window and added a do loop that runs until something else than y is entered.

--- Program.cs
+++ Program.cs
@@ -12,25 +12,42 @@ namespace HackerLook
         {
             int Height = Console.WindowHeight;
             int Width = Console.WindowWidth;
-            string Version = "Version 1.0";
+            string Version = "Version 1.2";
+            string Project = "Project";
+            string Descrption = "Describe your project";
 
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
-            //Use SetWindowSize so we can control the size of the window
-            //Console.SetWindowSize(70, 30);
-            //It is called Character Map to create the layout.
-            //Console.SetCursorPosition(10, 2);
-            Console.WriteLine("------------------------------------------------------------------------------");
-            Console.WriteLine("             ##   ##  #######  #######  ##   ##  #######  #######             ");
-            Console.WriteLine("             ##   ##  ##   ##  ##       ##  ##   ##       ##  ##              ");
-            Console.WriteLine("             #######  #######  ##       #####    #####    #####               ");
-            Console.WriteLine("             ##   ##  ##   ##  ##       ##  ##   ##       ##  ##              ");
-            Console.WriteLine("             ##   ##  ##   ##  #######  ##   ##  #######  ##   ##             ");
-            Console.WriteLine("------------------------------------------------------------------------------");
+
+            Console.WriteLine("*-----------------------------------------------------------------------------*");
+            Console.WriteLine("*             ##   ##  #######  #######  ##   ##  #######  #######            *");
+            Console.WriteLine("*             ##   ##  ##   ##  ##       ##  ##   ##       ##  ##             *");
+            Console.WriteLine("*             #######  #######  ##       #####    #####    #####              *");
+            Console.WriteLine("*             ##   ##  ##   ##  ##       ##  ##   ##       ##  ##             *");
+            Console.WriteLine("*             ##   ##  ##   ##  #######  ##   ##  #######  ##   ##            *");
+            Console.WriteLine("*-----------------------------------------------------------------------------*");
+            Console.WriteLine("                         ,--,                    ,--,                  ");
+            Console.WriteLine("     |__          __ ___/ /\\|                    |/\\ \\___ __           __|");
+            Console.WriteLine(" :__ |_|___/ __ ;(  )__, )                          ( ,__ (  ); __ \\___|_| __:");
+            Console.WriteLine(" /-\\|___/-\\_/  ; / /   '--;                        ;--'    \\ \\ ;  \\_/-\\___|/-\\");
+            Console.WriteLine(" \\ /    \\ /      \\      |                            |       /      \\ /    \\ /");
+            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
             Console.SetCursorPosition((Width-Version.Length)/2, 7);
             Console.WriteLine(Version);
-            Console.ReadKey();
+            Console.SetCursorPosition((Width-Project.Length)/2, 9);
+            Console.WriteLine(Project);
+            Console.SetCursorPosition((Width - Descrption.Length)/2, 10);
+            Console.WriteLine(Descrption);
+
+            Console.SetCursorPosition(0, 14);
+            do
+            {
+                Console.WriteLine("Test test");
+
+                Console.Write("Do you want to run the program again? y/n: ");
+            } while (Console.ReadLine() == "y");
         }
     }
 }
